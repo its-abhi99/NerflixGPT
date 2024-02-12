f2864bbd59b3ed7ec8d9107bad46cf5596a77999
its-abhi99 <abhi9074.s.kumar@gmail.com>
2024-02-13 00:44:26 +0530

    Add TopRated, Upcoming Movies API

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,14 +3,18 @@ import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
 import GptSearch from "./GptSearch";
 import { useSelector } from "react-redux";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
 
 const Browse = () => {
   const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
   useNowPlayingMovies();
   usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
 
   return (
     <div>
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,21 +1,22 @@
 import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
 
+
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
 
   return (
     movies.nowPlayingMovies && (
-      <div className="bg-black">
-        <div className=" mt-0 md:-mt-52 pl-4 md:pl-12 relative z-20">
+      <div className="bg-black scrollbar-hide">
+        <div className="-mt-52 pl-12 relative z-20 scrollbar-hide">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
           <MovieList title={"Popular"} movies={movies.popularMovies} />
           <MovieList
             title={"Upcoming Movies"}
-            movies={movies.nowPlayingMovies}
+            movies={movies.upcomingMovies}
           />
-          <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Horror"} movies={movies.topRatedMovies} />
         </div>
       </div>
     )
